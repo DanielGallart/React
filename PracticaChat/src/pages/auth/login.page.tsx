88e26b3baf80b68b87d8d8f029e4484ae96452ca
DanielGallart <danielgallart99@gmail.com>
2026-02-06 19:33:21 +0100

    funciones de login, register, loginWithGoogle y logout. Configuración tailwind y shadcn

--- PracticaChat/src/pages/auth/login.page.tsx
+++ PracticaChat/src/pages/auth/login.page.tsx
@@ -1,6 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { useAuthActions } from "../../hooks/use-auth-actions"
+
 const LoginPage = () => {
+  const {loginWithGoogle} = useAuthActions()
+
   return (
-    <div>Login Page</div>
-  )
+    <div>
+      <h1>Login</h1>
+      <Button onClick={loginWithGoogle}>Login with Google</Button>
+    </div>
+  );
 }
 export default LoginPage
